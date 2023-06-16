66876ec4373e18c66828b2a70e7a19cbdf03b4b2
bogdan <bogdandumitrescu@yahoo.com>
2023-06-16 13:09:04 -0600

    Now using render function to reference the html file rather than converting it to a string

--- challenges/views.py
+++ challenges/views.py
@@ -33,13 +33,10 @@ def monthly_challenge_by_number(request, month):
 
 
 def monthly_challenge(request, month):
-    # try:
+    try:
         challenge_text = monthly_challenges[month]
         return render(request, "challenges/challenge.html")
-        # response_data = f"<h1>{challenge_text}</h1"
-        response_data = render_to_string("challenges/challenge.html")
-        return HttpResponse(response_data)
-    # except:
+    except:
         return HttpResponseNotFound("<h1>This month is not supported!</h1>")
 
 def index(request):
