d5a959a75f902f11e0207019efd77bd1589c7275
bogdan <bogdandumitrescu@yahoo.com>
2023-06-16 14:38:04 -0600

    Added dynamic list of months. Ignoring idea and pycache

--- challenges/views.py
+++ challenges/views.py
@@ -42,15 +42,10 @@ def monthly_challenge(request, month):
         return HttpResponseNotFound("<h1>This month is not supported!</h1>")
 
 def index(request):
-    list_items = ""
     months = list(monthly_challenges.keys())
-    
-    response_data = ""
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
-        
-    response_data = f"<ul>{list_items}</ul>"
+
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
         
     return HttpResponse(response_data)
