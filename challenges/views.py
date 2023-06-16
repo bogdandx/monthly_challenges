b6ace073ce7a0083a5e85bc80298514b8d4d872b
bogdan <bogdandumitrescu@yahoo.com>
2023-06-16 13:17:15 -0600

    Switched to dynamic content in the monthly challenges

--- challenges/views.py
+++ challenges/views.py
@@ -35,7 +35,9 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return render(request, "challenges/challenge.html")
+        return render(request, "challenges/challenge.html", {
+            "text": challenge_text
+        })
     except:
         return HttpResponseNotFound("<h1>This month is not supported!</h1>")
 
