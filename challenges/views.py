5bccf488645c6adba9153c6a7eb45aff5b1927d3
bogdan <bogdandumitrescu@yahoo.com>
2023-06-16 14:48:20 -0600

    Added conditional logic to handle months without challenges

--- challenges/views.py
+++ challenges/views.py
@@ -15,7 +15,7 @@ monthly_challenges = {
     "september": "Learn Django for at least 20 minutes every day!",
     "october": "Eat no meat for the entire month!",
     "november": "Walk for at least 20 minutes every day!",
-    "december": "Learn Django for at least 20 minutes every day!"
+    "december": None
 }
 
 
