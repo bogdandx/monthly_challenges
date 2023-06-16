cc69b86e851393d00a6b614f8e3feaec15151c89
bogdan <bogdandumitrescu@yahoo.com>
2023-06-16 16:05:30 -0600

    Added navigation header. Changed 404 to use dedicated html page

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 from django.template.loader import render_to_string
 
@@ -39,7 +39,7 @@ def monthly_challenge(request, month):
             "text": challenge_text
         })
     except:
-        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
+        raise Http404()
 
 def index(request):
     months = list(monthly_challenges.keys())
